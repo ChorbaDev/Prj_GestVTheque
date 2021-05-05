39cd5736336d5de45165f856909c60c4e4e960a9
Dhoulnoun <60320772+Dhoulnoun@users.noreply.github.com>
2021-05-05 16:49:49 +0200

    path changing

--- src/Vue/Main.java
+++ src/Vue/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("SceneLogin.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
