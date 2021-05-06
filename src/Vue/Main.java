b059610489168dda123efbd072c315ddf013b8a4
Dhoulnoun <60320772+Dhoulnoun@users.noreply.github.com>
2021-05-06 22:36:24 +0200

    Create Scène commande

--- src/Vue/Main.java
+++ src/Vue/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("SceneLogin.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("SceneClient2.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
