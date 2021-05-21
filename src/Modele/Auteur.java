d7302355895aa4fd3456febda7ebceae266f291b
Dhoulnoun <60320772+Dhoulnoun@users.noreply.github.com>
2021-05-21 23:40:46 +0200

    commit bete
    fjeifejfief

--- src/Modele/Auteur.java
+++ src/Modele/Auteur.java
@@ -7,6 +7,8 @@ import javafx.beans.property.StringProperty;
 
 import java.util.Objects;
 
+// je suis très fort
+
 public class Auteur extends Personne{
     private IntegerProperty idAuteur;
     private StringProperty resume;
